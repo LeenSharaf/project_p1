1df6da38fc7fac7eb2ea735b147a45ce4216a4b0
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2023-11-25 02:00:21 +0200

    func_data.py

--- venv/catalog/func_data.py
+++ venv/catalog/func_data.py
@@ -9,6 +9,7 @@ class CatalogManager:
         with open(self.filename, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             return list(reader)
+    
 
     def save(self):
         with open(self.filename, 'w', newline='') as csvfile:
@@ -16,3 +17,34 @@ class CatalogManager:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(self.catalog)
+
+    def get_item_by_id(self, item_id):
+            for item in self.catalog:
+             if int(item['ID']) == item_id:
+                return item
+            return None       
+     
+    def update_stock(self, item_id, new_stock):
+        for item in self.catalog:
+            if int(item['ID']) == item_id:
+                item['stock'] = new_stock
+                self.save()
+                return item
+        return None
+
+    def update_cost(self, item_id, new_cost):
+        for item in self.catalog:
+            if int(item['ID']) == item_id:
+                item['cost'] = new_cost
+                self.save()
+                return item
+        return None
+
+    def update_stock_and_cost(self, item_id, new_stock, new_cost):
+        for item in self.catalog:
+            if int(item['ID']) == item_id:
+                item['stock'] = new_stock
+                item['cost'] = new_cost
+                self.save()
+                return item
+        return None
