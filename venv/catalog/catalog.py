7b95ff3dc41f1846ea6a5c42d61b2fb5e22f009f
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2023-11-25 02:03:26 +0200

    catalog.py

--- venv/catalog/catalog.py
+++ venv/catalog/catalog.py
@@ -1,53 +1,78 @@
-from flask import Flask, request, jsonify
 
+from flask import Flask, request, jsonify
+import csv
+from func_data import CatalogManager
 app = Flask(__name__)
  ###### URL####
 front = "http://localhost:5000"
 order = "http://localhost:5002"
+catalog_manager = CatalogManager()
+catalog_data = catalog_manager.read()
 
-Book = {
-    1: {"title": "How to get a good grade in DOS in 40 minutes a day", "stock": 10, "cost": 70, "topic": "distributed systems"},
-    2: {"title": "RPCs for Noobs", "stock": 5, "cost": 80, "topic": "distributed systems"},
-    3: {"title": "Xen and the Art of Surviving Undergraduate School", "stock": 1, "cost": 75, "topic": "undergraduate school"},
-    4: {"title": "Cooking for the Impatient Undergrad", "stock": 18, "cost": 60, "topic": "undergraduate school"},
-}
-###### query-by-subject ######
+### query-by-subject ######
 ####### GET http://localhost:5001/query-by-subject/TopicName #####
 
 @app.route('/query-by-subject/<topic>', methods=['GET'])
 def query_by_subject(topic):
-    res = [Item for Item in Book.values() if topic.lower() in Item['topic'].lower()]
+    res = [Item for Item in catalog_data if topic.lower() in Item['topic'].lower()]
     return jsonify({"items": res})
-
+    
 ###### query-by-item ######
 ####### GET http://localhost:5001/query-by-item/item_number #####
+@app.route('/query-by-item/<int:item_id>', methods=['GET'])
+def query_by_item(item_id):
+    item = catalog_manager.get_item_by_id(item_id)
 
-@app.route('/query-by-item/<int:item_number>', methods=['GET'])
-def query_by_item(item_number):
-
-    if item_number not in Book:
+    if item is None:
         return jsonify({"Error": "Not found"}), 404
-    
-### I found book #####
-    Item = Book[item_number]
-    return jsonify({"title": Item['title'], "stock": Item['stock'], "cost": Item['cost'], 
-                    "topic": Item['topic']})
+
+    return jsonify({
+        "title": item['Title'],
+        "stock": item['stock'],
+        "cost": item['cost'],
+        "topic": item['topic']
+    })
 
 ####### Update #####
-#### Put: http://localhost:5001/update/item_number ####
+#### Put: http://localhost:5001/update-stock/item_number ####
 
-@app.route('/update/<int:item_number>', methods=['PUT'])
-def update(item_number):
-    if item_number not in Book:
+@app.route('/update-stock/<int:item_id>', methods=['PUT'])
+def update_stock(item_id):
+    new_stock = request.json.get('new_stock')
+    updated_item = catalog_manager.update_stock(item_id, new_stock)
+
+    if updated_item:
+        return jsonify({"Message": "Stock updated successfully","item": updated_item})
+    else:
         return jsonify({"Error": "Not found"}), 404
     
-    data = request.get_json()
+####### Update #####
+#### Put:  http://localhost:5001/update-cost/item_number ####
 
-    if 'cost' in data:
-        Book[item_number]['cost'] = data['cost']
-    if 'stock' in data:
-        Book[item_number]['stock'] += data['stock']
-    return jsonify({"Message": "Updated Successful"})
+@app.route('/update-cost/<int:item_id>', methods=['PUT'])
+def update_cost(item_id):
+    new_cost = request.json.get('new_cost')
+    updated_item = catalog_manager.update_cost(item_id, new_cost)
+
+    if updated_item:
+        return jsonify({"Message": "Cost updated successfully","item": updated_item})
+    else:
+        return jsonify({"Error": "Not found"}), 404
+    
+####### Update #####
+#### Put:  http://localhost:5001/update-stock-and-cost/item_number ####
+
+@app.route('/update-stock-and-cost/<int:item_id>', methods=['PUT'])
+def update_stock_and_cost(item_id):
+    new_stock = request.json.get('new_stock')
+    new_cost = request.json.get('new_cost')
+    updated_item = catalog_manager.update_stock_and_cost(item_id, new_stock, new_cost)
+
+    if updated_item:
+        return jsonify({"Message": "Stock and Cost updated successfully", "item": updated_item})
+    else:
+        return jsonify({"Error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     app.run(port = 5001, debug = True)
